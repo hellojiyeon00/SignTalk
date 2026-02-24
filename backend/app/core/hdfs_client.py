fc0d26364d49a3f7e3ac7ec5fedc16d4319df76f
hellojiyeon00 <hellojiyeon00@gmail.com>
2026-02-24 17:35:56 +0900

    sign: hadoop 연결부 구현

--- backend/app/core/hdfs_client.py
+++ backend/app/core/hdfs_client.py
@@ -5,12 +5,11 @@ from app.core.config import settings
 # 전역 HDFS 클라이언트
 hdfs_client = None
 
-def get_hdfs():
+async def get_hdfs():
     """HDFS 클라이언트 반환"""
     global hdfs_client
 
     if hdfs_client is None:
-        # hdfs_client = InsecureClient(settings.HDFS_URL, user=settings.HDFS_USER)
-        hdfs_client = InsecureClient(settings.HDFS_URL)
+        hdfs_client = InsecureClient(settings.HDFS_URL, user=settings.HDFS_USER, timeout=5)
 
     return hdfs_client
