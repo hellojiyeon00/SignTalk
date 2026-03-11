6468c7c9be5653dc96b9ca67302121e348408866
hellojiyeon00 <hellojiyeon00@gmail.com>
2026-03-11 17:54:36 +0900

    sign: 백엔드 서버 주소 변경

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,4 +1,4 @@
 // 백엔드 서버 주소(프론트엔드에서 API 호출 시 사용)
 
-const BASE_URL = "http://127.0.0.1:8000"
-// const BASE_URL = "https://56.155.47.51:8951";
+// const BASE_URL = "http://127.0.0.1:8000"
+const BASE_URL = "https://56.155.47.51:8951";
