01e5eba93fe7f1bffa9aa2f08b2d62f4ca0a8cc4
SoyoungKim <growinloop@gmail.com>
2026-03-13 10:30:55 +0900

    text: rename temp to Hadoop

--- temp/hadoop_app/api/hdfs.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Body
-
-from hadoop_app.services.hdfs_service import f_save_hdfs, C_Hadoop
-
-router = APIRouter()
-
-@router.post("/save_hdfs")
-async def save_hdfs(data: C_Hadoop):
-    return await f_save_hdfs(data)
